da4637344f3b3a6fdfedeb088a1701982c60f8ee
marwajomaa <marwajomaa4@gmail.com>
2021-04-28 01:41:42 +0300

    fix cors issue

--- client/pages/user/login.js
+++ client/pages/user/login.js
@@ -25,6 +25,11 @@ export default function login() {
         headers: {
           "Content-Type": "application/json",
           "Abp.TenantId": "32",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers":
+            "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
         },
       });
 
