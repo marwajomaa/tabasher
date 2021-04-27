c2eefa254995dbf51a7ebdb3e76fc01779a3bf93
marwajomaa <marwajomaa4@gmail.com>
2021-04-28 02:01:39 +0300

    fix cors issue

--- client/pages/user/login.js
+++ client/pages/user/login.js
@@ -26,11 +26,14 @@ export default function login() {
         headers: {
           "Content-Type": "application/json",
           "Abp.TenantId": "32",
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin":
+            "*,http://127.0.0.1:3000, https://api.tabasher.dev/",
           "Access-Control-Allow-Methods":
             "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Origin, Content-Type",
-          mode: "cors",
+          "Access-Control-Allow-Headers":
+            "Origin, Content-Type, Authorization, Abp.TenantId",
+          "Access-Control-Request-Headers":
+            "Content-Type,Origin, Authorization",
         },
       });
 
